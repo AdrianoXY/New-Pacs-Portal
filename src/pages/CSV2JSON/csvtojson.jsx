9a58a95788c23666eb4b58a7a1dbb533e938a8bd
ApolloChen <117019832+AdrianoXY@users.noreply.github.com>
2023-05-25 14:41:27 +0800

    csvtojson

--- src/pages/CSV2JSON/csvtojson.jsx
+++ src/pages/CSV2JSON/csvtojson.jsx
@@ -1,7 +1,53 @@
 import React, { useState, useEffect } from "react";
+import axios from "../../axios/axios";
 
 const Manage = () => {
-  const [progressPercentage, setProgressPercentage] = useState(71);
+  const [progressPercentage, setProgressPercentage] = useState([]);
+  const [sample, setSample] = useState([]);
+  const [file, setFile] = useState([]);
+
+  const Conversion = (PID, SID, fileID,index) => {
+    axios
+      .post("/api/csvtojson", JSON.stringify({ PID, SID, fileID }), {
+        onUploadProgress: function (e) {
+          const percentComplete = (e.loaded / e.total) * 0;
+          console.log(percentComplete);
+
+          setProgressPercentage((prevProgressPercentages) => {
+            const updatedProgressPercentages = [...prevProgressPercentages];
+            updatedProgressPercentages[index] = percentComplete;
+            return updatedProgressPercentages;
+          });
+        },
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          console.log("Fail");
+        } else {
+          alert("Fail");
+        }
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get(`/api/sample`)
+      .then((res) => {
+        setSample(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(`/api/file`)
+      .then((res) => {
+        setFile(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div class="grid h-screen w-screen grid-cols-9 grid-rows-6 overflow-auto">
@@ -32,34 +78,55 @@ const Manage = () => {
           <table class="mt-5 w-[95%] table-auto">
             <thead>
               <tr>
-                <th class=" border-b-4">No.</th>
+                <th class=" border-b-4">PID</th>
+                <th class=" border-b-4">SID</th>
                 <th class=" border-b-4">FileName</th>
-                <th class=" border-b-4">Status</th>
                 <th class=" border-b-4">Process Rate(complete/total)</th>
                 <th class=" border-b-4">Operation</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th class="border-t-2 ">1</th>
-                <th class="border-t-2 ">123456</th>
-                <th class="border-t-2 ">72212</th>
-                <th class="border-t-2 ">
-                  <div class="flex w-full items-center justify-center">
-                    <div className="h-3 w-52 rounded-md bg-gray-300 ">
-                      <div
-                        style={{ width: `${progressPercentage}%` }}
-                        className={`h-full rounded-md ${
-                          progressPercentage < 70
-                            ? "bg-red-600"
-                            : "bg-green-600"
-                        }`}
-                      />
-                    </div>
-                  </div>
-                </th>
-                <th class="border-t-2 ">DNA</th>
-              </tr>
+              {file.map((file, index) => {
+                const extension = file.filename.split(".").pop().toLowerCase();
+
+                if (extension !== "csv") {
+                  return null;
+                }
+
+                const pidArray = sample.map((item) => item.PID);
+
+                return (
+                  <tr key={index}>
+                    <th class="border-t-2 ">{pidArray[index]}</th>
+                    <th class="border-t-2 ">{file.SID}</th>
+                    <th class="border-t-2 ">{file.filename}</th>
+                    <th class="border-t-2 ">
+                      <div class="flex w-full items-center justify-center">
+                        <div class="h-3 w-52 rounded-md bg-gray-300 ">
+                          <div
+                            style={{ width: `${progressPercentage}%` }}
+                            class={`h-full rounded-md ${
+                              progressPercentage < 70
+                                ? "bg-red-600"
+                                : "bg-green-600"
+                            }`}
+                          />
+                        </div>
+                      </div>
+                    </th>
+                    <th class="border-t-2">
+                      <button
+                        class="h-10 w-36 rounded-md"
+                        onClick={() =>
+                          Conversion(pidArray[index], file.SID, file._id, index)
+                        }
+                      >
+                        conversion
+                      </button>
+                    </th>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
